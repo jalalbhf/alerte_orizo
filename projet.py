18ba5d843be5db64b7896e5b7619da28fa5759d2
jalalbhf <150329566+jalalbhf@users.noreply.github.com>
2024-04-01 14:12:31 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -1,9 +1,10 @@
+# On a commenceé par l'importation des modules dont on avait besoin en passant par Flask
 from flask import Flask, rendertemplate, request, jsonify
-
+# Du coup on a initialisé Flask
 app = Flask(name)
 
 events = []
-
+# A définit la route principale et on renvois au contenu de la page en index.html lorsque l'on accède a la racine
 @app.route('/')
 def index():
     return rendertemplate('index.html')
@@ -13,7 +14,7 @@ def addevent():
     event_data = request.get_json()
     events.append(event_data)
     return jsonify({'message': 'Event added successfully'})
-
+# Route pour obtenir la liste des événements
 @app.route('/get_events', methods=['GET'])
 def get_events():
     return jsonify(events)
